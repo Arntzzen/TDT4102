02c9d959bd4cae4ae59903fca06ae3bb22184eae
Arntzen <markus.arntzen@gmail.com>
2026-01-23 18:20:23 +0100

    Fredig med oppg. 1 på øving 2

--- oving2/main.cpp
+++ oving2/main.cpp
@@ -6,25 +6,38 @@
 
 void inputAndPrintInteger() {
     // BEGIN: 1b
-
+    int tall;
+    cout << "Skriv inn et tall: ";
+    cin >> tall;
+    cout << "du skrev tallet " << tall << endl;
     // END: 1b
 }
 
 int inputInteger() {
     // BEGIN: 1c
-    return 0;
+    int tall2;
+    cout << "Skriv inn et tall: ";
+    cin >> tall2;
+    return tall2;
     // END: 1c  
 }
 
 void printSumOfInputIntegers() {
     // BEGIN: 1d
-
+    int tall1 = inputInteger();
+    int tall2 = inputInteger();
+    int sum = tall1 + tall2;
+    cout << "Summen av tallene: " << sum << endl;
     // END: 1d
 }
 
 bool isOdd(int n) {
     // BEGIN: 1f
-    return 0;
+    if ((n % 2) == 0) {
+        return false;
+    } else {
+        return true;
+    }
     // END: 1f
 }
 
@@ -94,9 +107,43 @@ void printBalance(vector<int> balanceVec) {
     // END: 5b
 }
 
+void menu() {
+	while (1) {
+		std::cout << "Menu\n"
+			<< "0.\t quit\n"
+			<< "1.\t Skriv ut et tall - void\n"
+			<< "2.\t Skriv ut et tall - int\n"
+            << "3.\t Summer to tall\n"
+			<< "Input: ";
+
+		int menu_choice;
+		std::cin >> menu_choice;
+		
+		switch (menu_choice)
+		{
+		case 1:
+			inputAndPrintInteger();
+			break;
+        case 2: {
+            int number = inputInteger();
+            cout << "Du skrev tallet " << number << endl;
+            break;
+        }
+        case 3: {
+            printSumOfInputIntegers();
+            break;
+        }
+		default:
+			return; // quit program
+			break;
+		}
+	}
+}
+
 int main() {
     // Oppgave 1a
     // Her kan du teste funksjonene dine ved hjelp av menysystem som beskrevet. 
     // NB: Denne delen av koden blir IKKE automatisk rettet.
+    menu();
     return 0;
 }
